42e1f2021986314689d0430d0fb3c74635ef3cc5
Bert Spaan <bert.spaan@gmail.com>
2015-07-06 17:57:42 +0200

    install script now uses homebrew to check neo4j dir

--- install.sh
+++ install.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
+export TARGET=`brew ls --verbose neo4j | grep plugins/README.txt | sed s/README\.txt//`
+
 mvn package
 
 neo4j stop
-cp target/*.jar /usr/local/Cellar/neo4j/2.2.2/libexec/plugins
+cp target/*.jar $TARGET
 neo4j start
+
+echo Installed plugin into $TARGET
+
 
 curl http://localhost:7474/db/data/ext/ExpandConcepts
